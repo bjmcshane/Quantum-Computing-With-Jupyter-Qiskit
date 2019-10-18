0aae49ea4aa0d47af18966b07e789e9e80f12fdc
bmcshane <bmcshane@iu.edu>
2019-10-18 13:59:41 -0400

    Chapter01

---
+++ .ipynb_checkpoints/test_py_ver-checkpoint.py
@@ -0,0 +1,53 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import sys\n",
+    "# The following code tests your Python installation\n",
+    "\n",
+    "try:\n",
+    "\timport numpy\n",
+    "\timport matplotlib\n",
+    "except:\n",
+    "\traise Exception(\"make sure to pip install -r requirements.txt\")\n",
+    "\n",
+    "\n",
+    "\n",
+    "if not (sys.version_info > (3, 0)):\n",
+    "\traise Exception(\"You are running Python 2. Python 3.4+ is required for code examples\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.7.3"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
